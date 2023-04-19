17ff14e55b2822304474c1604851489cffda6e5a
MDC911360 <15910609156@163.com>
2023-04-19 18:19:27 +0800

    添加 app 页面

---
+++ app/index.js
@@ -0,0 +1,10 @@
+var Generator = require("yeoman-generator");
+
+module.exports = class extends Generator {
+//   hello() {
+//     this.log('hello ...');
+//   }
+    method(){
+        console.log("测试成功")
+    }
+};
